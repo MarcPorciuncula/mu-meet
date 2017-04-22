6ab76d14f6dafcfdd1d5e14421c11e4569c7bfcc
Marc Porciuncula <marcporciuncula@gmail.com>
2017-04-22 10:48:51 +1000

    init

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAgq_qbft78WR_bXEyiQl7z2fSInz4fBZE',
+  authDomain: 'meetingsync-f62e3.firebaseapp.com',
+  databaseURL: 'https://meetingsync-f62e3.firebaseio.com',
+  projectId: 'meetingsync-f62e3',
+  storageBucket: 'meetingsync-f62e3.appspot.com',
+  messagingSenderId: '569083161786',
+};
+
+firebase.initializeApp(config);
