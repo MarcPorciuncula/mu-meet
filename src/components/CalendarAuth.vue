90977d005d09a75f1941b3b0344e244d1e359de2
Unknown <kalanavithana1@gmail.com>
2017-04-22 21:53:12 +1000

    Finalising Drafts of Pages

--- src/components/CalendarAuth.vue
+++ src/components/CalendarAuth.vue
@@ -2,6 +2,8 @@
   <div>
     <p class='center head'>please select the calendars you wish to schedule around</p>
     <p class='center' style='font-size:16px; margin-top:0px;'>make sure your uni schedule is synced with one of these!</p>
+
+    <p class='help center'><a href="https://my.monash.edu.au/news-and-events/calendar-feeds/direct.html">I haven't synced my calendar yet, help!</a></p>
   </div>
 </template>
 
@@ -18,5 +20,10 @@
   padding-top: 5px;
   font-size: 22px;
   margin-bottom: 5px;
+}
+
+.help {
+  font-size: 14px;
+  color: #90A4AE;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Referral from './components/Referral.vue';
+import Output from './components/Output.vue';
 import router from './router';
 import store from './store';
 import KeenUI from 'keen-ui';
@@ -18,6 +18,6 @@ new Vue({
   el: '#app',
   store,
   router,
-  template: '<Referral/>',
-  components: { Referral },
+  template: '<Output/>',
+  components: { Output },
 });
