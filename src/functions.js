b6d8d0b4e5aa3b262eb90fc8f8acce136ade44b6
Marc Porciuncula <marcporciuncula@gmail.com>
2017-05-29 02:03:14 +1000

    WIP add a staging env

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import firebase from 'firebase';
+import { config } from '@/firebase.config';
 
-const FUNCTIONS_URI =
-  'https://us-central1-meetingsync-f62e3.cloudfunctions.net/';
+const FUNCTIONS_URI = `https://us-central1-${config.projectId}.cloudfunctions.net/`;
 
 export async function functions(fnName, options = {}) {
   const headers = Object.assign({}, options.headers);
