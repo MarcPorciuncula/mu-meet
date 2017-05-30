395a1179c99fa8b76e2078c4deb14f9d0e52e96b
Marc Porciuncula <marcporciuncula@gmail.com>
2017-05-30 23:29:39 +1000

    Indicate client build for production or staging

--- build/build.js
+++ build/build.js
@@ -10,7 +10,7 @@ var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
 
-var spinner = ora('building for production...')
+var spinner = ora(`building for ${process.env.NODE_ENV === 'production' ? 'production' : 'staging'}...`)
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
