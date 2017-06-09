29f3c68e8328d56c8c9a3b6a6e1340f52b7923bd
Marc Porciuncula <marcporciuncula@gmail.com>
2017-06-09 19:23:13 +1000

    Fix import capitalization typo

--- src/router/dashboard.js
+++ src/router/dashboard.js
@@ -1,4 +1,4 @@
-import Dashboard from '@/components/dashboard';
+import Dashboard from '@/components/Dashboard';
 import ProfileBadge from '@/components/ProfileBadge';
 import store from '@/store';
 
