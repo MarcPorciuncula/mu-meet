92561c698cbafcd798167ff11f17ac6bb613dfd2
Marc Porciuncula <marcporciuncula@gmail.com>
2017-06-27 13:41:21 +1000

    Use eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,26 +10,11 @@ module.exports = {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: ['standard', 'prettier'],
   // required to lint *.vue files
   plugins: ['html'],
   // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    semi: ['error', 'always'],
-    'space-before-function-paren': [
-      2,
-      {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always',
-      },
-    ],
-    'comma-dangle': [2, 'always-multiline'],
   },
 };
