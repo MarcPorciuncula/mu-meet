fc54028ab4de478dc7c60e289bc4e1b6d106a717
Marc Porciuncula <marcporciuncula@gmail.com>
2017-07-03 17:24:34 +1000

    Fix double calendar subscription

--- src/router/calendars.js
+++ src/router/calendars.js
@@ -4,7 +4,7 @@ import { SUBSCRIBE_CALENDARS } from '@/store/actions';
 import store from '@/store';
 
 async function beforeEnter(to, from, next) {
-  if (!Object.keys(store.getters[IS_SUBSCRIBED_CALENDARS]).length) {
+  if (!store.getters[IS_SUBSCRIBED_CALENDARS]) {
     await store.dispatch(SUBSCRIBE_CALENDARS);
   }
   next();
