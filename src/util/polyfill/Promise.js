b34103b1a7b9de1c2fdee6e2e7479b40c54ab8fc
Marc Porciuncula <marcporciuncula@gmail.com>
2017-10-19 21:43:09 +1100

    Only force polyfill Promise in production build

--- src/util/polyfill/Promise.js
+++ src/util/polyfill/Promise.js
@@ -1,12 +1,12 @@
 // Disable native promises, (the webpack config will polyfill them back in)
 // Native promises do not support the unhandledrejection event, so we need to use
 // the polyfilled version to track unhandled promise rejections
-if (window.Promise) {
+if (process.env.NODE_ENV === 'production' && window.Promise) {
   console.debug(
     `This browser provides a native Promise. I'm disabling it to force use of core-js to allow "onunhandledrejection".`,
   );
   delete window.Promise;
-}
 
-const Promise = require('core-js/library/es6/promise');
-window.Promise = Promise;
+  const Promise = require('core-js/library/es6/promise');
+  window.Promise = Promise;
+}
